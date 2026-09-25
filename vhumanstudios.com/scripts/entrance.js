971cb2468ff764968507151e8cbe9ffada29c525
An <56708851+an-vu@users.noreply.github.com>
2026-09-25 07:35:17 -0500

    Delete vhumanstudios.com directory

--- vhumanstudios.com/scripts/entrance.js
+++
@@ -1,5 +0,0 @@
-// Choose once per page load; resizing or scrolling never reselects the entrance.
-function createStudioEntrance(brand) {
-  const entrances = [createDotEntrance, createTypingEntrance, createShuffleEntrance];
-  return entrances[Math.floor(Math.random() * entrances.length)](brand);
-}
